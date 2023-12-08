fd66e8b2d789269d998708a47487ce8455c55257
Victor Volkov <vityan2504@gmail.com>
2023-12-08 18:28:52 +0300

    Реализовал получение и отправку данных через fetch

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const getData = () => {
+   return fetch('db.json').then(response => response.json());
+};
+
+const sendData = (url, data) => {
+   return fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+         'Content-type': 'application/json; charset=UTF-8',
+      },
+   })
+}
+
+getData().then(data => {
+   sendData('https://jsonplaceholder.typicode.com/posts', data)
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+}).catch(err => console.log(err))
+
